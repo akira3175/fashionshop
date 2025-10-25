0dc7c37d0794558976124b28fcba055ff4864318
Duy Le <l.a.d11052003@gmail.com>
2025-10-25 14:53:49 +0700

    Show size product

--- core/views.py
+++ core/views.py
@@ -1,6 +1,6 @@
 # core/views.py
 from django.shortcuts import render
-from products.models import Product, Category
+from products.models import Product, Category, Size
 from django.core.paginator import Paginator
 def home(request):
     categories = Category.objects.filter(hide=False)
@@ -8,9 +8,11 @@ def home(request):
     paginator = Paginator(products, 12)
     page_number = request.GET.get("page")
     page_obj = paginator.get_page(page_number)
+    sizes = Size.objects.all()
     return render(request, "home.html", {
         "categories": categories,
         "products": products,
         "page_obj": page_obj,
+        "sizes": sizes,
     })
 
