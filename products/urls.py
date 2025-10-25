f3da55f4220a0dc8abeb787afe5bf647ebab4c9b
Duy Le <l.a.d11052003@gmail.com>
2025-10-25 14:12:50 +0700

    Add detail product and page home

--- products/urls.py
+++ products/urls.py
@@ -3,7 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.product_list, name='home'),           # /products/
-    path('<int:product_id>/', views.product_detail, name='product_detail'),  # /products/1/
     path('category/<int:category_id>/', views.product_by_category, name='product_by_category')
 ]
 
